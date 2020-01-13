0eef0861d4a38fe48c7a8f67d6f7b3ca4e5547a7
dotero-spark <dotero@sparkdigital.com>
2020-01-13 19:48:16 -0300

    testing commit

--- Exercises/Conditional_Statements.js
+++ Exercises/Conditional_Statements.js
@@ -1,11 +1,16 @@
+var number_1 = 3;
+var number_2 = 2;
+
 function conditionalStatements(number_1, number_2){
     if(number_1 > number_2){
-        document.writeln(number_1, " is greater than ", number_2, ".");
+        console.log(number_1, " is greater than ", number_2, ".");
     }
     else if(number_1 < number_2){
-        document.writeln(number_2, " is greater than ", number_1, ".");
+        console.log(number_2, " is greater than ", number_1, ".");
     }
     else{
-        document.writeln("Both numbers are the same.");
+        console.log("Both numbers are the same.");
     }
 }
+
+conditionalStatements(number_1, number_2);
